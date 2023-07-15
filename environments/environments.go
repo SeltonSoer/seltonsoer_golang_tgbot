6b3f3d85a19091a4bbaf28843f2c76f1621a2bcd
Danilenko Aleksey <setsurensa@gmail.com>
2023-07-15 09:54:05 +0300

    create environments tg_key

--- environments/environments.go
+++ environments/environments.go
@@ -8,22 +8,10 @@ import (
 func GetEnvironments() (string, error) {
 	var tgKey string
 
-	appEnv := os.Getenv("APP_ENV")
-
-	if appEnv == "local" {
-		tgKey = os.Getenv("TG_DEV_KEY")
-		if tgKey == "" {
-			return "", errors.New("переменная окружения TG_DEV_KEY не задана")
-		}
-		return tgKey, nil
-	} else if appEnv == "prod" {
-		tgKey = os.Getenv("TG_PROD_KEY")
-		if tgKey == "" {
-			return "", errors.New("переменная окружения TG_PROD_KEY не задана")
-		}
-		return tgKey, nil
-	} else {
-		return "", errors.New("переменная окружения APP_ENV не задана")
+	tgKey = os.Getenv("TG_KEY")
+	if tgKey == "" {
+		return "", errors.New("переменная окружения TG_KEY не задана")
 	}
+	return tgKey, nil
 
 }
